91e15ca922c7eca8302c3366a61e278465fd6f2a
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-11-19 15:23:02 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title('SERVER TEST')
+st.title('PRIMER DASHBOARD')
 st.write("""Upload file!""")
 uploaded_file = st.file_uploader('Excel')
 if uploaded_file is not None:
