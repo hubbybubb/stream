5b715627d6e5e4d367e5e7e69d6cc741ab993922
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-11-22 11:21:47 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 
 st.title('Primer Dashboard-a')
-st.write("""Upload file!""")
 path = (r'T:\DB\Drilling\01.Departman za tehniku i tehnologiju proizvodnje nafte i gasa\STPNiG\1. Analitika\Streamlit\Bas.xlsx')
 df = pd.read_excel(r''+path+'')
 x = df.x
