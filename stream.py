7b8e5a0844628208d2f3effb3078536bef5d1466
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-11-22 10:22:39 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -4,14 +4,19 @@ import plotly.express as px
 
 st.title('Primer Dashboard-a')
 st.write("""Hehehe""")
-uploaded_file = 'T:\DB\Drilling\01.Departman za tehniku i tehnologiju proizvodnje nafte i gasa\STPNiG\1. Analitika\Streamlit\1.xlsx'
-df = pd.read_excel(uploaded_file)
-x = df.x
-y1 = df.y1
-y12 = df.y12
-y2 = df.y2
-y22 = df.y22
-fig1 = px.line(y=[y1,y12], x=x, template='plotly_dark', title='Среднее количество нефти в резервуарах', labels={'value': 'Среднее количество нефти в резервуарах',
+uploaded_file = st.file_uploader('Excel')
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    try:
+        x = df.x
+        y1 = df.y1
+        y12 = df.y12
+        y2 = df.y2
+        y22 = df.y22
+
+        fig1 = px.line(y=[y1,y12], x=x, template='plotly_dark', title='Среднее количество нефти в резервуарах',
+                       labels={
+                           'value': 'Среднее количество нефти в резервуарах',
                            'x': 'Дата',
                            'wide_variable_0':'Nafta',
                            'wide_variable_1':'Nafta2',
@@ -19,14 +24,16 @@ fig1 = px.line(y=[y1,y12], x=x, template='plotly_dark', title='Среднее к
                        })
 
         
-st.write(fig1)
+        st.write(fig1)
 
 
-fig2 = px.line(y=[y2,y22], x=x, template='plotly_dark', title='Добыча нефти',
+        fig2 = px.line(y=[y2,y22], x=x, template='plotly_dark', title='Добыча нефти',
                        labels={
                            'value': 'Добыча нефти',
                            'x': 'Дата',
                            'variable': 'Proizvodnja'
                        })
-st.write(fig2)
-
+        st.write(fig2)
+        
+    except:
+        st.write('Try again!')
