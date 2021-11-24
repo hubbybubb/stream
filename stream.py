9658b843c1ee6eb507d7568a63ceaa720ce724e9
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-11-24 10:17:02 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 def clicky():
     if genre == 'Уклањање асфалтно-смоластих и парафинских наслага':
-        st.baloons
+        st.balloons
         
     else:
         st.write('Netacan odgovor!')
@@ -27,37 +27,37 @@ option = st.selectbox('Odaberite naftno polje: ',
 
 st.write('Odabrali ste naftno polje ', option)
 
-st.write("""Upload file!""")
-uploaded_file = st.file_uploader('Excel')
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
-    try:
-        x = df.x
-        y1 = df.y1
-        y12 = df.y12
-        y2 = df.y2
-        y22 = df.y22
+# st.write("""Upload file!""")
+# uploaded_file = st.file_uploader('Excel')
+# if uploaded_file is not None:
+#     df = pd.read_excel(uploaded_file)
+#     try:
+#         x = df.x
+#         y1 = df.y1
+#         y12 = df.y12
+#         y2 = df.y2
+#         y22 = df.y22
 
-        fig1 = px.line(y=[y1,y12], x=x, template='plotly_dark', title='Среднее количество нефти в резервуарах', line_shape='spline',
-                       labels={
-                           'value': 'Среднее количество нефти в резервуарах',
-                           'x': 'Дата',
-                           'wide_variable_0':'Nafta',
-                           'wide_variable_1':'Nafta2',
-                           'variable': 'Proizvodnja'
-                       })
+#         fig1 = px.line(y=[y1,y12], x=x, template='plotly_dark', title='Среднее количество нефти в резервуарах', line_shape='spline',
+#                        labels={
+#                            'value': 'Среднее количество нефти в резервуарах',
+#                            'x': 'Дата',
+#                            'wide_variable_0':'Nafta',
+#                            'wide_variable_1':'Nafta2',
+#                            'variable': 'Proizvodnja'
+#                        })
 
         
-        st.write(fig1)
+#         st.write(fig1)
 
 
-        fig2 = px.line(y=[y2,y22], x=x, template='plotly_dark', title='Добыча нефти',line_shape='spline',
-                       labels={
-                           'value': 'Добыча нефти',
-                           'x': 'Дата',
-                           'variable': 'Proizvodnja'
-                       })
-        st.write(fig2)
+#         fig2 = px.line(y=[y2,y22], x=x, template='plotly_dark', title='Добыча нефти',line_shape='spline',
+#                        labels={
+#                            'value': 'Добыча нефти',
+#                            'x': 'Дата',
+#                            'variable': 'Proizvodnja'
+#                        })
+#         st.write(fig2)
         
-    except:
-        st.write('Try again!')
+#     except:
+#         st.write('Try again!')
