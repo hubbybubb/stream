2fa772372f7f7eac9ab9afcd7545709028bc99d0
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-12-16 11:22:53 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -19,4 +19,6 @@ df = dataframe1.drop_duplicates(subset ="Name",
                      keep = False)
 x = st.selectbox("Meni",df)
 st.write(x)
-rslt_df = dataframe1[dataframe1['Name'].isin(str(x))]
+y = []
+y.append(x)
+rslt_df = dataframe1[dataframe1['Name'].isin(y)]
