b147cba920087d7bbe0e645c9dbef5c804e1ca8d
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-12-16 12:32:12 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -32,5 +32,6 @@ st.table(rslt_df2)
 st.write(str(rslt_df2.tacni))
 st.write(str(rslt_df2.netacni))
 
-l = ["Osnove", "Zakonska", "Mehanicka"]
+table = rslt_df2.T.tail(5)
+st.write(table)
 fig1=px.bar(rslt_df2, x=l, y=l ,orientation="v",title="<b>Pracenje uspesnosti po testu</b>",color_discrete_sequence=["#0083B8"]*len(m),template="plotly_white",)
