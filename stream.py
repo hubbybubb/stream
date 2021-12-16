131fa9c9b5e3566137afea83814651b143175d8b
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-12-16 12:34:51 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -14,7 +14,7 @@ from PIL import Image
 
 import plotly.figure_factory as ff
 
-dataframe1 = pd.read_excel('1.xlsx')
+dataframe1 = pd.read_excel('1.xlsx', index=False)
 df = dataframe1.drop_duplicates(subset ="Name")
 x = st.selectbox("Meni",df)
 st.write(x)
