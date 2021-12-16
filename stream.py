92f181181f77785d6ed05745077b3604ca8a2087
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-12-16 12:59:17 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -33,6 +33,7 @@ st.write(str(rslt_df2.tacni))
 st.write(str(rslt_df2.netacni))
 
 table = rslt_df2.T.tail(3)
-st.write(table)
+st.write(table.to_string(index=False))
 
-print(table.to_string(index=False))
+
+
