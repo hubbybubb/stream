c5641dbb396532c796825ddb00ed7a72f9d91d0a
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-12-16 13:07:47 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -33,9 +33,7 @@ st.write(str(rslt_df2.tacni))
 st.write(str(rslt_df2.netacni))
 
 table = rslt_df2.T.tail(3)
-st.write(table.Osnove)
 
-fig1=px.bar(date,x="Datum",y="tacni",orientation="v",title="<b>Pracenje uspesnosti po testu</b>",color_discrete_sequence=["#0083B8"]*len(m),template="plotly_white",)
+st.write(str(table.Osnove))
 
-
-
+fig1=px.bar(date,x="Datum",y="tacni",orientation="v",title="<b>Pracenje uspesnosti po testu</b>",color_discrete_sequence=["#0083B8"]*len(m),template="plotly_white")
