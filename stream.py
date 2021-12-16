6383e3d5349dfbe52d91546f6ff7b39117a6bb8e
hubbybubb <94691986+hubbybubb@users.noreply.github.com>
2021-12-16 14:34:25 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -24,14 +24,11 @@ def admin():
   rslt_df = dataframe1[dataframe1['Name'].isin(y)]
   st.table(rslt_df)
   x1 = rslt_df.Datum
-  st.write()
   x2 = st.selectbox("Meni",x1)
   y2=[]
   y2.append(x2)
   rslt_df2 = rslt_df[rslt_df['Datum'].isin(y2)]
   st.table(rslt_df2)
-  st.write(str(rslt_df2.tacni))
-  st.write(str(rslt_df2.netacni))
 
   table = rslt_df2
 
